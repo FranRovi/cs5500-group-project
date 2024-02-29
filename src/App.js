2d3cee07a42b19de0e3d33a0a6b86416bca1cdc8
yanginto1996 <yanginto@gmail.com>
2024-02-29 01:01:22 -0800

    working nav bar that takes us to about us and homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Homepage from './components/homepage';
+import Navbar from './components/navbar';
 
 function App() {
   return (
-    <Homepage />
-
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,21 +1,24 @@
+import { Link } from 'react-router-dom';
 import { Button } from '@mui/material';
-import { TextField } from '@mui/material';
-
 
 // Import Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-transparent">
+        <nav className="navbar navbar-expand-lg navbar-light bg-transparent" style={{ outline: 'none' }}>
             <div className="container-fluid">
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <form className="d-flex">
-                        <Button variant="outlined" style={{ color: 'black' }}>Home</Button>
-                        <Button variant="outlined" style={{ color: 'black' }}>About Us</Button>
+                        <Link to="/">
+                            <Button variant="text" style={{ color: 'black' }}>Homepage</Button>
+                        </Link>
+                        <Link to="/about">
+                            <Button variant="text" style={{ color: 'black' }}>About Us</Button>
+                        </Link>
                     </form>
                 </div>
             </div>
